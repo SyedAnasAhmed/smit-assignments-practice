7d3ed4691793169374e41a065ae12b2cb12d110c
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-08-16 14:34:54 +0500

    all projects added

---
+++ jsassignments/assignment1js/app.js
@@ -0,0 +1,15 @@
+alert("hello world")
+
+
+var num1 = 25;
+var result = num1-- + num1++ + num1++ - --num1 - --num1 - num1-- + num1;
+console.log(result);
+
+var num2 = 38;
+var result2 = ++num2 + ++num2 - --num2 - num2 + num2;
+console.log(result2)
+
+
+var num3 = 8;
+var result3 = num3 + num3 + ++num3 + --num3 - --num3 + ++num3 + --num3 - num3;
+console.log(result3)
---
+++ jsassignments/assignment2js(objects)/app.js
@@ -0,0 +1,17 @@
+// alert("hello");
+
+
+                // CHAPTER 1 ASSIGNMENT
+
+// 1. Write a script to greet your website visitor using JS alert box.
+
+// alert("ASSALAMUALAIKUM !");
+
+// 2. Write a script to display following message on your web page:
+
+// alert("Error! Please enter a valid password")
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+// alert("Welcome to js land... Happy Coding!")
+
